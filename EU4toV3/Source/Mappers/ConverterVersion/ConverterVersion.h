48c65c996f4d2baa8dc3d33afaa0b4bc2323d2df
Ivo Elezović <ivo@element.hr>
2021-05-23 17:49:02 +0200

    more cleanup

--- EU4toV3/Source/Mappers/ConverterVersion/ConverterVersion.h
+++ EU4toV3/Source/Mappers/ConverterVersion/ConverterVersion.h
@@ -5,21 +5,25 @@
 
 namespace mappers
 {
-	class ConverterVersion : commonItems::parser
-	{
-	public:
-		ConverterVersion();
-	 explicit ConverterVersion(std::istream& theStream);
+class ConverterVersion: commonItems::parser
+{
+  public:
+	ConverterVersion();
+	explicit ConverterVersion(std::istream& theStream);
 
-		friend std::ostream& operator<<(std::ostream& output, const ConverterVersion& versionParser);
+	[[nodiscard]] const auto& getName() const { return name; }
+	[[nodiscard]] const auto& getVersion() const { return version; }
+	[[nodiscard]] const auto& getDescription() const { return descriptionLine; }
 
-	private:
-		void registerKeys();
+	friend std::ostream& operator<<(std::ostream& output, const ConverterVersion& versionParser);
 
-		std::string name;
-		std::string version;
-		std::string descriptionLine;
-	};
-}
+  private:
+	void registerKeys();
+
+	std::string name;
+	std::string version;
+	std::string descriptionLine;
+};
+} // namespace mappers
 
 #endif // CONVERTER_VERSION_H
