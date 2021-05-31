ad97a6dea924c71769dbe5ebe1c741f150bcb3e7
Ivo Elezović <ivo@element.hr>
2021-05-31 21:55:05 +0200

    country tests

--- EU4toV3Tests/EU4WorldTests/CountryManagerTests/EU4CountryTests.cpp
+++ EU4toV3Tests/EU4WorldTests/CountryManagerTests/EU4CountryTests.cpp
@@ -5,6 +5,7 @@
 #include <gmock/gmock-matchers.h>
 using testing::ElementsAre;
 using testing::UnorderedElementsAre;
+#include "UnitTypeLoader/UnitTypeLoader.h"
 
 TEST(EU4World_EU4CountryTests, countryTagIsStored)
 {
@@ -529,4 +530,296 @@ TEST(EU4World_EU4CountryTests, NamesAndAdjectivesUseBaseGameForFallbackForEnglis
 	EXPECT_EQ("the adjective", country.getAdjective("russian"));
 	EXPECT_EQ("the name", country.getName("german"));
 	EXPECT_EQ("the adjective", country.getAdjective("portugese"));
+}
+
+TEST(EU4World_EU4CountryTests, NationalColorsCanBeLoadedInTheirFullGlory)
+{
+	std::stringstream input;
+	input << "colors={\n";
+	input << "	map_color = { 1 2 3 }\n";
+	input << "	revolutionary_colors = { 4 5 6 }\n";
+	input << "	custom_colors = {\n";
+	input << "		flag = 7\n";
+	input << "		color = 23\n";
+	input << "		symbol_index = 34\n";
+	input << "		flag_colors = {12 34 56}\n";
+	input << "	}\n";
+	input << "}\n";
+	const EU4::Country country("TAG", input);
+
+	const auto& symbol = country.getNationalColors();
+
+	EXPECT_EQ("= rgb { 1 2 3 }", symbol.getMapColor()->outputRgb());
+	EXPECT_EQ("= rgb { 4 5 6 }", symbol.getRevolutionaryColor()->outputRgb());
+	EXPECT_EQ(8, symbol.getCustomColors()->flagIndex);
+	EXPECT_EQ(24, symbol.getCustomColors()->colorIndex);
+	EXPECT_EQ(35, symbol.getCustomColors()->symbolIndex);
+	EXPECT_EQ("= rgb { 12 34 56 }", symbol.getCustomColors()->flagColors->outputRgb());
+}
+
+TEST(EU4World_EU4CountryTests, BotanicalExpeditionGrabsLastDynasty)
+{
+	std::stringstream input;
+	input << "history={\n";
+	input << "	1265.1.1={\n";
+	input << "		monarch={\n";
+	input << "			dynasty=\"fancyskirt\"\n";
+	input << "		}\n";
+	input << "	}\n";
+	input << "	1765.1.1={\n";
+	input << "		monarch={\n";
+	input << "			dynasty=\"fancykilt\"\n";
+	input << "		}\n";
+	input << "	}\n";
+	input << "	1768.1.1={\n";
+	input << "		monarch={\n";
+	input << "			dynasty=\"fancypants\"\n";
+	input << "		}\n";
+	input << "	}\n";
+	input << "}\n";
+	const EU4::Country country("TAG", input);
+
+	const auto& historicalEntry = country.getHistoricalEntry();
+
+	EXPECT_EQ("fancypants", historicalEntry.lastDynasty);
+	EXPECT_TRUE(historicalEntry.monarchy);
+}
+
+TEST(EU4World_EU4CountryTests, BotanicalExpeditionFlipsMonarchyForRepublics)
+{
+	std::stringstream input;
+	input << "government = { government = republic }\n";
+	input << "history={\n";
+	input << "	1768.1.1={\n";
+	input << "		monarch={\n";
+	input << "			dynasty=\"fancypants\"\n";
+	input << "		}\n";
+	input << "	}\n";
+	input << "}\n";
+	const EU4::Country country("TAG", input);
+
+	const auto& historicalEntry = country.getHistoricalEntry();
+
+	EXPECT_EQ("fancypants", historicalEntry.lastDynasty);
+	EXPECT_FALSE(historicalEntry.monarchy);
+}
+
+TEST(EU4World_EU4CountryTests, BotanicalExpeditionFlipsMonarchyForTheocracies)
+{
+	std::stringstream input;
+	input << "government = { government = theocracy }\n";
+	input << "history={\n";
+	input << "	1768.1.1={\n";
+	input << "		monarch={\n";
+	input << "			dynasty=\"fancypants\"\n";
+	input << "		}\n";
+	input << "	}\n";
+	input << "}\n";
+	const EU4::Country country("TAG", input);
+
+	const auto& historicalEntry = country.getHistoricalEntry();
+
+	EXPECT_EQ("fancypants", historicalEntry.lastDynasty);
+	EXPECT_FALSE(historicalEntry.monarchy);
+}
+
+TEST(EU4World_EU4CountryTests, CultureInCoresCanBePingedForCoresUnderForeignRule)
+{
+	std::stringstream provinceInput; // this province belongs to TAG, but it's a core of GAT and has their primary culture.
+	provinceInput << "culture = cul\n";
+	provinceInput << "religion = rel\n";
+	provinceInput << "owner = TAG\n";
+	auto province = std::make_shared<EU4::Province>("-1", provinceInput);
+
+	std::stringstream countryManagerInput;
+	countryManagerInput << "TAG = { primary_culture = cul2 }\n"; // they don't share the culture with the province
+	countryManagerInput << "GAT = { primary_culture = cul }\n";
+	const EU4::CountryManager countryManager(countryManagerInput);
+
+	const auto& tag = countryManager.getCountry("TAG");
+	const auto& gat = countryManager.getCountry("GAT");
+	tag->addProvince(province);
+	gat->addCore(province);
+
+	DatingData datingData;
+	datingData.startEU4Date = date("1444.11.11");
+	province->buildPopRatios(datingData);
+
+	EXPECT_TRUE(gat->cultureSurvivesInCores(countryManager.getCountries()));
+}
+
+TEST(EU4World_EU4CountryTests, CultureInCoresCanBePingedForCoresUnderSameCultureRule)
+{
+	std::stringstream provinceInput; // this province belongs to TAG, but it's a core of GAT and has their primary culture.
+	provinceInput << "culture = cul\n";
+	provinceInput << "religion = rel\n";
+	provinceInput << "owner = TAG\n";
+	auto province = std::make_shared<EU4::Province>("-1", provinceInput);
+
+	std::stringstream countryManagerInput;
+	countryManagerInput << "TAG = { primary_culture = cul }\n"; // they share the culture with Gat's province so it belongs to them, actually.
+	countryManagerInput << "GAT = { primary_culture = cul }\n";
+	const EU4::CountryManager countryManager(countryManagerInput);
+
+	const auto& tag = countryManager.getCountry("TAG");
+	const auto& gat = countryManager.getCountry("GAT");
+	tag->addProvince(province);
+	gat->addCore(province);
+
+	DatingData datingData;
+	datingData.startEU4Date = date("1444.11.11");
+	province->buildPopRatios(datingData);
+
+	EXPECT_FALSE(gat->cultureSurvivesInCores(countryManager.getCountries()));
+}
+
+TEST(EU4World_EU4CountryTests, CountryWeightSumsProvinceWeight)
+{
+	std::stringstream province1Input;
+	province1Input << "owner = TAG\n";
+	province1Input << "base_tax = 3\n";
+	province1Input << "base_production = 4\n";
+	province1Input << "base_manpower = 5\n";
+	auto province1 = std::make_shared<EU4::Province>("-1", province1Input);
+
+	std::stringstream province2Input;
+	province2Input << "owner = TAG\n";
+	province2Input << "base_tax = 13\n";
+	province2Input << "base_production = 14\n";
+	province2Input << "base_manpower = 15\n";
+	auto province2 = std::make_shared<EU4::Province>("-2", province2Input);
+
+	const EU4::BuildingCostLoader buildings;
+
+	province1->determineProvinceWeight(buildings);
+	province2->determineProvinceWeight(buildings);
+
+	std::stringstream input;
+	EU4::Country country("TAG", input);
+
+	country.addProvince(province1);
+	country.addProvince(province2);
+
+	EXPECT_NEAR(12, province1->getProvinceWeight(), 0.0001);
+	EXPECT_NEAR(42, province2->getProvinceWeight(), 0.0001);
+	EXPECT_NEAR(54, country.getCountryWeight(), 0.0001);
+}
+
+TEST(EU4World_EU4CountryTests, UpdateRegimentTypesUpdatesallArmies)
+{
+	EU4::UnitTypeLoader unitTypeLoader;
+	std::stringstream unitInput;
+	unitInput << "type = heavy_ship maneuver = 60\n";
+	unitTypeLoader.loadUnitType("the-manowar", unitInput);
+	std::stringstream unitInput2;
+	unitInput2 << "type = transport maneuver = 4\n";
+	unitTypeLoader.loadUnitType("the-transport", unitInput2);
+	std::stringstream unitInput3;
+	unitInput3 << "type = infantry maneuver = 2\n";
+	unitTypeLoader.loadUnitType("native_indian_archer", unitInput3);
+	std::stringstream unitInput4;
+	unitInput4 << "type = cavalry maneuver = 4\n";
+	unitTypeLoader.loadUnitType("native_indian_horseperson", unitInput4);
+
+	std::stringstream input;
+	input << "navy={\n";
+	input << "	name = \"1st Fleet\"\n";
+	input << "	location = 11\n";
+	input << "	ship = {name = \"Dulecha\" home = 1211 type = \"the-manowar\"}\n";
+	input << "	ship = {name = \"Koneba\" home = 1211 type = \"the-manowar\"}\n";
+	input << "	ship = {name = \"Dallol\" home = 1211 type = \"the-transport\"}\n";
+	input << "}\n";
+	input << "army={\n";
+	input << "	name = \"1st Army\"\n";
+	input << "	location = 971\n";
+	input << "	regiment = {name = \"Penobscott's 1st Regiment\" home = 971 type = \"native_indian_archer\"}\n";
+	input << "	regiment = {name = \"Penobscott's 3st Regiment\" home = 971 type = \"native_indian_archer\"}\n";
+	input << "	regiment = {name = \"Penobscott's 2st Regiment\" home = 971 type = \"native_indian_horseperson\"}\n";
+	input << "}\n";
+
+	EU4::Country country("TAG", input);
+
+	country.updateRegimentTypes(unitTypeLoader);
+
+	const auto& navy = country.getArmies()[0];
+	const auto& army = country.getArmies()[1];
+
+	EXPECT_EQ(120, navy.getTotalTypeStrength("heavy_ship")); // two heavy ships worth 60 each
+	EXPECT_EQ(4, navy.getTotalTypeStrength("transport"));		// one transport worth 4.
+	EXPECT_EQ(4, army.getTotalTypeStrength("infantry"));		// two inf regiments worth 2 each
+	EXPECT_EQ(4, army.getTotalTypeStrength("cavalry"));		// one cav regiment worth 4
+}
+
+TEST(EU4World_EU4CountryTests, EatCountryWontEatItself)
+{
+	std::stringstream input;
+	auto country = std::make_shared<EU4::Country>("TAG", input);
+
+	std::stringstream province1Input;
+	const auto province1 = std::make_shared<EU4::Province>("-1", province1Input);
+
+	country->addProvince(province1);
+	country->eatCountry(country);
+
+	EXPECT_FALSE(country->getProvinces().empty()); // province is still inside.
+}
+
+TEST(EU4World_EU4CountryTests, EatCountryFullExample)
+{
+	std::stringstream provincesInput;
+	provincesInput << "-1 = { cores={EAT} }\n"; // to eater
+	provincesInput << "-2 = { cores={EAT} }\n"; // to eater
+	provincesInput << "-3 = { cores={TGT} }\n"; // to target
+	EU4::ProvinceManager provinceManager;
+	provinceManager.loadProvinces(provincesInput);
+
+	std::stringstream countryInput;
+	countryInput << "TGT = { army={} }\n"; // target, tech 0 for simplicity, 1 army, 0 score
+	countryInput << "EAT = { navy={} \n";	// eater, tech 10, 1 navy, 10 score
+	countryInput << "	technology = { adm_tech = 10 dip_tech = 10 mil_tech = 10}\n";
+	countryInput << " age_score = { 10 }\n";
+	countryInput << "}\n";
+	EU4::CountryManager countryManager(countryInput);
+
+	const auto& target = countryManager.getCountry("TGT");
+	const auto& eater = countryManager.getCountry("EAT");
+
+	// weight ratio will be 2:1
+	target->addCore(provinceManager.getProvince(1));
+	target->addProvince(provinceManager.getProvince(1));
+	eater->addCore(provinceManager.getProvince(2));
+	eater->addProvince(provinceManager.getProvince(2));
+	eater->addCore(provinceManager.getProvince(3));
+	eater->addProvince(provinceManager.getProvince(3));
+
+	// yum
+	eater->eatCountry(target);
+
+	// eater's score has been lowered by 2:1 from 10 to 6.66 since target was at 0
+	EXPECT_NEAR(6.6666, eater->getScore(), 0.0001);
+	// techs have been lowered by same ratio.
+	EXPECT_NEAR(6.6666, eater->getAdmTech(), 0.0001);
+	EXPECT_NEAR(6.6666, eater->getDipTech(), 0.0001);
+	EXPECT_NEAR(6.6666, eater->getMilTech(), 0.0001);
+
+	// eaten province contains a single core - of the eater
+	EXPECT_THAT(provinceManager.getProvince(1)->getCores(), UnorderedElementsAre("EAT"));
+
+	// eaten province has been added to the cores and provinces of eater
+	EXPECT_EQ(1, eater->getCores()[2]->getID());
+	EXPECT_EQ(1, eater->getProvinces()[2]->getID());
+	EXPECT_EQ(3, eater->getCores().size());
+	EXPECT_EQ(3, eater->getProvinces().size());
+
+	// target has no cores or provinces.
+	EXPECT_TRUE(target->getCores().empty());
+	EXPECT_TRUE(target->getProvinces().empty());
+
+	// eater has both armies
+	EXPECT_EQ(2, eater->getArmies().size());
+	EXPECT_TRUE(eater->getArmies()[0].getArmyFloats());
+	EXPECT_FALSE(eater->getArmies()[1].getArmyFloats()); // eaten land army
+
+	// target has no armies
+	EXPECT_TRUE(target->getArmies().empty());
 }
