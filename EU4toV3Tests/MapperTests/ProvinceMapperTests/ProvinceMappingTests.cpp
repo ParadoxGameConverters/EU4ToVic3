4729b6bcb4220cc97bdc7aa811cec35dfc2c2a47
Ivo Elezovic <ivo.elezovic@gmail.com>
2022-11-15 09:10:03 +0100

    pr review

--- EU4toV3Tests/MapperTests/ProvinceMapperTests/ProvinceMappingTests.cpp
+++ EU4toV3Tests/MapperTests/ProvinceMapperTests/ProvinceMappingTests.cpp
@@ -2,11 +2,19 @@
 #include "gtest/gtest.h"
 #include <gmock/gmock-matchers.h>
 
+TEST(Mappers_ProvinceMappingTests, MappingsDefaultToEmpty)
+{
+	std::stringstream input;
+	const mappers::ProvinceMapping theMapper(input);
+
+	EXPECT_TRUE(theMapper.getEU4Provinces().empty());
+	EXPECT_TRUE(theMapper.getV3Provinces().empty());
+}
+
 TEST(Mappers_ProvinceMappingTests, EU4ProvincesCanBeAdded)
 {
 	std::stringstream input;
-	input << "= { eu4 = 2 eu4 = 1 }";
-
+	input << "eu4 = 2 eu4 = 1";
 	const mappers::ProvinceMapping theMapper(input);
 
 	EXPECT_THAT(theMapper.getEU4Provinces(), testing::UnorderedElementsAre(1, 2));
@@ -15,8 +23,7 @@ TEST(Mappers_ProvinceMappingTests, EU4ProvincesCanBeAdded)
 TEST(Mappers_ProvinceMappingTests, Vic33ProvincesCanBeAdded)
 {
 	std::stringstream input;
-	input << "= { vic3 = x654321 vic3 = x123456 }";
-
+	input << "vic3 = x654321 vic3 = x123456";
 	const mappers::ProvinceMapping theMapper(input);
 
 	EXPECT_THAT(theMapper.getV3Provinces(), testing::UnorderedElementsAre("x654321", "x123456"));
