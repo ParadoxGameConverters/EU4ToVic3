1b9da205c0a6835ac5eae99b7e9a98e7b9a78e00
Ivo Elezović <ivo@element.hr>
2022-12-03 12:25:15 +0100

    .. clang

--- EU4toV3/Source/V3World/ClayManager/ClayMapTypedefs.h
+++ EU4toV3/Source/V3World/ClayManager/ClayMapTypedefs.h
@@ -11,9 +11,9 @@ class Country;
 namespace V3
 {
 class Province;
-using ProvinceMap = std::map<std::string, std::shared_ptr<Province>>;					 // v3 province name->v3 province
-using StateToProvinceMap = std::map<std::string, ProvinceMap>;								 // state name -> v3 provinces
-using SourceOwners = std::map<std::string, std::shared_ptr<EU4::Country>>;				 // eu4tag, eu4country
+using ProvinceMap = std::map<std::string, std::shared_ptr<Province>>;		// v3 province name->v3 province
+using StateToProvinceMap = std::map<std::string, ProvinceMap>;					// state name -> v3 provinces
+using SourceOwners = std::map<std::string, std::shared_ptr<EU4::Country>>; // eu4tag, eu4country
 } // namespace V3
 
 #endif // CLAY_MAP_TYPEDEFS_H
