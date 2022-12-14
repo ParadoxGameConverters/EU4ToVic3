68072213125204df607e572edb2d849f84fa3499
Gawquon <gawquonthestoryteller@gmail.com>
2022-12-14 06:00:17 -0500

    Terrain Modifiers

---
+++ EU4toV3/Source/V3World/Loaders/TerrainLoader/TerrainModifierLoader.cpp
@@ -0,0 +1,42 @@
+#include "TerrainModifierLoader.h"
+#include "CommonRegexes.h"
+#include "ParserHelpers.h"
+#include <Log.h>
+
+void V3::TerrainModifierLoader::loadTerrainModifiers(const std::string& filePath)
+{
+	registerKeys();
+	parseFile(filePath);
+	clearRegisteredKeywords();
+}
+
+void V3::TerrainModifierLoader::loadTerrainModifiers(std::istream& theStream)
+{
+	registerKeys();
+	parseStream(theStream);
+	clearRegisteredKeywords();
+}
+
+void V3::TerrainModifierLoader::registerKeys()
+{
+	registerRegex(commonItems::catchallRegex, [this](const std::string& terrain, std::istream& theStream) {
+		for (const auto& [type, multiplier]: commonItems::assignments(theStream).getAssignments())
+		{
+			try
+			{
+				if (type == "priority")
+				{
+					stateTerrainModifiers[terrain] = std::stod(multiplier);
+				}
+				else
+				{
+					buildingTerrainModifiers[terrain][type] = std::stod(multiplier);
+				}
+			}
+			catch (const std::exception& e)
+			{
+				Log(LogLevel::Error) << "Failed read " << type << " under " << terrain << " modifier: " << e.what();
+			}
+		}
+	});
+}
