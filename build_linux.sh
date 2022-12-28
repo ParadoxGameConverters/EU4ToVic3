d746065c22f56580ea03e2895a73a882fcf45c51
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-12-28 15:32:24 +0100

    Build frontend by local Linux build script (#253)
    * Use dotnet publish for local Linux build
    
    * update build_linux_foreign.sh
    
    * update windows_build_test_and_push.yml
    
    * Build frontend

--- build_linux.sh
+++ build_linux.sh
@@ -11,6 +11,10 @@ cmake --build build -- -j40  &&
 mv Release-Linux ../Release/EU4ToVic3 &&
 cd .. &&
 
+cd Fronter.NET &&
+dotnet publish -p:PublishProfile=linux-x64 --output:"../Release" &&
+cd .. &&
+
 cp EU4toV3/Data_Files/*yml Release/Configuration/ &&
 cp EU4toV3/Data_Files/fronter*txt Release/Configuration/ &&
 
