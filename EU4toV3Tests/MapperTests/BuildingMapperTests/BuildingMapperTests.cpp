e2400c1c563b0ab23918f73f33c039bfdf1a3868
Gawquon <gawquonthestoryteller@gmail.com>
2022-12-31 12:32:22 -0500

    BuildingMapper

---
+++ EU4toV3Tests/MapperTests/BuildingMapperTests/BuildingMapperTests.cpp
@@ -0,0 +1,12 @@
+#include "BuildingMapper/BuildingMapper.h"
+#include "gtest/gtest.h"
+#include <gmock/gmock-matchers.h>
+
+TEST(Mappers_ColonialRegionMapperTests, rulesCanBeLoadedInOrder)
+{
+	mappers::BuildingMapper mapper;
+	mapper.loadBuildingMappings("TestFiles/configurables/buildings_map.txt");
+
+	EXPECT_THAT(mapper.getVic3Buildings("wharf"), testing::UnorderedElementsAre("building_fishing_wharf"));
+	EXPECT_THAT(mapper.getVic3Buildings("mills"), testing::UnorderedElementsAre("building_glassworks", "building_paper_mills", "building_logging_camp"));
+}
---
+++ EU4toV3Tests/MapperTests/BuildingMapperTests/BuildingMappingTests.cpp
@@ -0,0 +1,15 @@
+#include "BuildingMapper/BuildingMapping.h"
+#include "gtest/gtest.h"
+#include <gmock/gmock-matchers.h>
+
+TEST(Mappers_BuildingMappingTests, RulesCanBeLoaded)
+{
+
+	std::stringstream input;
+	input << "eu4 = building_1 vic3 = { building_1, building_2 }\n";
+
+	const mappers::BuildingMapping buildingMapping(input);
+
+	EXPECT_THAT(buildingMapping.getVic3Buildings(), testing::UnorderedElementsAre("building_1", "building_2"));
+	EXPECT_EQ("building_1", buildingMapping.getEU4Building());
+}
