54228feca1ca622cfd2908e3e548efdb617db5e6
Ivo Elezovic <ivo.elezovic@gmail.com>
2023-01-23 10:49:27 +0100

    optimize wf

--- EU4toV3/Copy_Files_Debug.bat
+++ EU4toV3/Copy_Files_Debug.bat
@@ -5,6 +5,8 @@ copy "Data_Files\configuration-example.txt" "..\Debug\EU4ToVic3\"
 copy "Resources\msvcp140_codecvt_ids.dll" "..\Debug\EU4ToVic3\"
 copy "Resources\vcruntime140_1.dll" "..\Debug\EU4ToVic3\"
 copy "Resources\rakaly.dll" "..\Debug\EU4ToVic3\"
+copy "Resources\VC_redist.x64.exe" "..\Debug\"
+copy "Data_Files\GeneralFAQ-READ.ME.FIRST.txt" "..\Debug\"
 
 mkdir "..\Debug\Configuration"
 copy "Data_Files\fronter-configuration.txt" "..\Debug\Configuration\"
