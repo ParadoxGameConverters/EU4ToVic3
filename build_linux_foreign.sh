a9b5b380fca6553470003943b770a167b3f8f16f
Zemurin <ivo@element.hr>
2023-02-04 13:37:02 +0100

    cmakes

--- build_linux_foreign.sh
+++ build_linux_foreign.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
+# Keep at 11 due to issues with ranges in gcc12 (?)
 export CC=/usr/bin/gcc-11 &&
 export CXX=/usr/bin/g++-11 &&
 
-rm -rf Release/ &&
-cd Fronter &&
-./build_linux.sh &&
-mv Release ../ &&
-cd ../EU4toV3 &&
-rm -rf build &&
-rm -rf Release-Linux &&
 cmake -H. -Bbuild &&
-cmake --build build -- -j3  &&
-mv Release-Linux ../Release/EU4ToVic3 &&
-cd .. &&
-
-cp EU4toV3/Data_Files/*yml Release/Configuration/ &&
-cp EU4toV3/Data_Files/fronter*txt Release/Configuration/ &&
-
-tar -cjf EU4ToVic3-release.tar.bz2 Release
+cmake --build build -- -j5
