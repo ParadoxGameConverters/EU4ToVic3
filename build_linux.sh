0391d4ca998da029df8fedb6fbedd9137ec1f6cb
Zemurin <ivo@element.hr>
2023-02-05 15:32:09 +0100

    clean

--- build_linux.sh
+++ build_linux.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Keep at 11 due to issues with ranges in gcc12 (?)
-export CC=/usr/bin/gcc-11 &&
-export CXX=/usr/bin/g++-11 &&
-
-rm -rf build &&
-rm -rf Release-Linux &&
-rm -rf test &&
 cmake -H. -Bbuild &&
 cmake --build build -- -j40
 
--- build_linux_foreign.sh
+++ build_linux_foreign.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Keep at 11 due to issues with ranges in gcc12 (?)
-export CC=/usr/bin/gcc-11 &&
-export CXX=/usr/bin/g++-11 &&
-
 cmake -H. -Bbuild &&
 cmake --build build -- -j5
