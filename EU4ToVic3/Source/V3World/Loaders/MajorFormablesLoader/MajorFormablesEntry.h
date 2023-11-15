037ae904edb837ba139cbed9eafa6c2a5ef46db6
Ivo Elezovic <ivo.elezovic@gmail.com>
2023-11-15 10:21:25 +0100

    colonials, tags, buildings, formables.

--- EU4ToVic3/Source/V3World/Loaders/MajorFormablesLoader/MajorFormablesEntry.h
+++ EU4ToVic3/Source/V3World/Loaders/MajorFormablesLoader/MajorFormablesEntry.h
@@ -11,6 +11,7 @@ class MajorFormablesEntry: commonItems::parser
 	explicit MajorFormablesEntry(std::istream& theStream);
 
 	[[nodiscard]] const auto& getStanzas() const { return stanzas; }
+	[[nodiscard]] const auto& getPossibleStanzas() const { return possibleStanzas; }
 	[[nodiscard]] const auto& getCultures() const { return cultures; }
 	[[nodiscard]] const auto& getRequiredTechnology() const { return requiredTechnology; }
 	void addCulture(const std::string& culture) { cultures.emplace(culture); }
@@ -24,7 +25,8 @@ class MajorFormablesEntry: commonItems::parser
 	parser filterParser;
 	std::string activeTag;
 
-	std::vector<std::string> stanzas; // unparsed stanzas.
+	std::vector<std::string> stanzas;			// unparsed stanzas.
+	std::vector<std::string> possibleStanzas; // unparsed stanzas in possible block.
 	std::set<std::string> cultures;
 	std::string requiredTechnology;
 };
