cf19d5fd215948d434405d900bb5c69247edc653
Ivo Elezović <ivo@element.hr>
2024-07-06 17:35:47 +0200

    DW pops fill.

--- EU4ToVic3/Source/Output/outPops/outPops.cpp
+++ EU4ToVic3/Source/Output/outPops/outPops.cpp
@@ -28,7 +28,7 @@ void outSubStatePops(std::ostream& output, const V3::SubState& subState)
 	output << "\t\tregion_state:" << subState.getOwner()->getTag() << " = {\n";
 	for (const auto& pop: subState.getSubStatePops().getPops())
 	{
-		if (pop.getSize() == 0)
+		if (pop.getSize() <= 0)
 			continue;
 		outPop(output, pop);
 	}
@@ -40,7 +40,7 @@ void outStatePops(std::ostream& output, const V3::State& state)
 	output << "\ts:" << state.getName() << " = {\n";
 	for (const auto& substate: state.getSubStates())
 	{
-		if (substate->getSubStatePops().getPopCount() == 0)
+		if (substate->getSubStatePops().getPopCount() <= 0)
 			continue;
 		outSubStatePops(output, *substate);
 	}
@@ -57,7 +57,7 @@ void OUT::exportPops(const std::string& outputName, const std::map<std::string, 
 	output << commonItems::utf8BOM << "POPS = {\n";
 	for (const auto& state: states | std::views::values)
 	{
-		if (state->getStatePopCount() == 0)
+		if (state->getStatePopCount() <= 0)
 			continue;
 		outStatePops(output, *state);
 	}
