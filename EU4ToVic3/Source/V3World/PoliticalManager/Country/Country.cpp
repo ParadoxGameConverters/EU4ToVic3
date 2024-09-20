f8c5a137e21ef55f5cb4e817f7a405ffbc02d1e2
Elfangor567 <gliamh@hotmail.com>
2024-09-20 03:06:33 -0400

    Dynamic New World Nation Formation (#582)
    - Converted colonial subjects can now form new world nations such as America, Brazil, etc.
    - All colonial nations should now convert as "colonial"-type nations.

--- EU4ToVic3/Source/V3World/PoliticalManager/Country/Country.cpp
+++ EU4ToVic3/Source/V3World/PoliticalManager/Country/Country.cpp
@@ -699,7 +699,12 @@ void V3::Country::determineWesternizationWealthAndLiteracy(double topTech,
 
 void V3::Country::determineCountryType()
 {
-	if (!processedData.westernized)
+	// Colonies should always be colonial, as it affects triggers
+	if (sourceCountry->isColony())
+	{
+		processedData.type = "colonial";
+	}
+	else if (!processedData.westernized)
 	{
 		// 20 is the cutoff so american natives that heavily invest into tech
 		// end up with more than 20 civLevel score and remain unrecognized.
@@ -713,11 +718,7 @@ void V3::Country::determineCountryType()
 	}
 	else
 	{
-		// civilized/westernized nations can either be recognized or colonial.
-		if (sourceCountry->isColony())
-			processedData.type = "colonial";
-		else
-			processedData.type = "recognized";
+		processedData.type = "recognized";
 	}
 }
 
