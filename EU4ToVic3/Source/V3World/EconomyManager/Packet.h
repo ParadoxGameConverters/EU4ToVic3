e0f03ed4456b0d6c4e1e51d2d1500b84758a4980
Gawquon <gawquonthestoryteller@gmail.com>
2024-11-12 17:22:46 -0500

    Document Packet.h

--- EU4ToVic3/Source/V3World/EconomyManager/Packet.h
+++ EU4ToVic3/Source/V3World/EconomyManager/Packet.h
@@ -3,6 +3,15 @@
 #include "Building/Building.h"
 #include "ClayManager/State/SubState.h"
 
+/* A Packet is the limiting factor which determines how many levels of a building a substate can buy in one iteration.
+ * This prevents each substate from being too dominated by one type of industry. The result is simply the minimum of 4 independent factors
+ *
+ * 1. Sector Limit (How many buildings does the country as a whole need of this type, as defined by a configuration file?)
+ * 2. Budgetary (How many levels can the substate afford by spending all of its points?)
+ * 3. Capacity (Is this building type limited by resource availability or current laws?)
+ * 4. Clustering (How much do we want industries to concentrate in fewer states? Set by configuration file.)
+ */
+
 namespace V3
 {
 class Packet
