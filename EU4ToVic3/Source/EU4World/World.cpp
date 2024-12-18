5159ed587690e07e2b332e06d7f32996947a5a4c
Ivo Elezovic <ivo.elezovic@gmail.com>
2024-12-18 14:44:26 +0100

    Reenabling TO

--- EU4ToVic3/Source/EU4World/World.cpp
+++ EU4ToVic3/Source/EU4World/World.cpp
@@ -220,16 +220,14 @@ void EU4::World::registerKeys(const std::shared_ptr<Configuration>& theConfigura
 			}
 			if (mod.name == "Third Odyssey: Back to the Motherland")
 			{
-				throw std::runtime_error("1.8 update broke Third Odyssey: Back to the Motherland support. We're working on re-enabling it.");
-				/*
-					Log(LogLevel::Notice) << "Third Odyssey: Back to the Motherland detected. Enabling TO support.";
-					theConfiguration->setTO();
-					if (theConfiguration->configBlock.euroCentric != Configuration::EUROCENTRISM::EuroCentric)
-					{
-						Log(LogLevel::Notice) << "TO is auto-enabling Eurocentric conversion.";
-						theConfiguration->setEurocentric();
-					}
-					*/
+				// throw std::runtime_error("1.8 update broke Third Odyssey: Back to the Motherland support. We're working on re-enabling it.");
+				Log(LogLevel::Notice) << "Third Odyssey: Back to the Motherland detected. Enabling TO support.";
+				theConfiguration->setTO();
+				if (theConfiguration->configBlock.euroCentric != Configuration::EUROCENTRISM::EuroCentric)
+				{
+					Log(LogLevel::Notice) << "TO is auto-enabling Eurocentric conversion.";
+					theConfiguration->setEurocentric();
+				}
 			}
 		}
 	});
